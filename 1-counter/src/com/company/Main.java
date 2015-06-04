f007ec10a9b693eb32bf757524e4eb506ba0e4f1
RKorolev <kororoma@mail.ru>
2015-06-04 12:45:30 +0300

    Project-2 finished
    4.6.2015

--- 1-counter/src/com/company/Main.java
+++ 1-counter/src/com/company/Main.java
@@ -3,21 +3,35 @@ package com.company;
 import java.*;
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner st= new Scanner(System.in);
         String[] a=new String[100];
         int i=0;
-        while(st.nextLine().length()>0) {
-            a[i] = st.nextLine();
+        int L=0;
+        int blockCount =0;
+        System.out.println("Enter text:");
+        while(true)
+        {
+            a[i] =st.nextLine();
+            L+=a[i].length();
+            blockCount += a[i].split(" +").length;
+            if(a[i].equals(""))
+            {break;}
             i++;
         }
-        i=0;
-        while(a[i]!=null) {
-            System.out.println(a[i]);
-            i++;
+
+        blockCount--;
+
+        for (int j = 0; j < i; j++) {
+            System.out.println(a[j]);
         }
+        System.out.println("There is "+L+" symbols");
+        System.out.println("There is "+i+" strings");
+        System.out.println("There is "+blockCount+" words");
 
     }
 }
