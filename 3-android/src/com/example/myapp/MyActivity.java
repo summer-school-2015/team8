9084207d2ef220126b103680a3e1ffb4262af5c9
MrVatnik <zacvars@gmail.com>
2015-06-06 13:38:35 +0300

    Finished prime numbers program

--- 3-android/src/com/example/myapp/MyActivity.java
+++ 3-android/src/com/example/myapp/MyActivity.java
@@ -7,59 +7,52 @@ import android.view.View;
 import android.widget.Toast;
 import android.widget.Button;
 
-public class MyActivity extends Activity{
+public class MyActivity extends Activity implements View.OnClickListener {
 
-    private int mix =0;
-    private int x1 =0;
-    private int x2 =0;
-    private int x3 =0;
-    private Button button1 = (Button)findViewById(R.id.button1);
-    private Button button2 = (Button)findViewById(R.id.button2);
-    private Button button3 = (Button)findViewById(R.id.button3);
-
-
+    private int n=0;
+    Button button1=null;
+    Button button2=null;
+    Button button3=null;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        int i=0;
-        int b=0;
-        while(i<Math.sqrt(mix)) {
-            if(mix%i==0) {
-                button1.setBackgroundColor(Color.GREEN);
-                button2.setBackgroundColor(Color.GREEN);
-                button3.setBackgroundColor(Color.GREEN);
-                b++;
-                break;
+
+        button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+
+        if( v instanceof Button) {
+            Button button = (Button) v;
+
+            int clicks = Integer.parseInt(button.getText().toString());
+            clicks++;
+            n++;
+
+            button.setText(String.valueOf(clicks));
+
+            int i;
+            int b = 0;
+            if (n == 1)
+                b=1;
+            for (i = 2; i <= Math.sqrt(n); i++) {
+                    if (n % i == 0) {
+                        b=1;
+                        break;
+                    }
+                }
+            int col=Color.GREEN;
+
+            if (b == 0)
+                col=Color.BLUE;
+            button1.setBackgroundColor(col);
+            button2.setBackgroundColor(col);
+            button3.setBackgroundColor(col);
             }
-            i++;
-            if(b==0)
-            {
-                button1.setBackgroundColor(Color.BLUE);
-                button2.setBackgroundColor(Color.BLUE);
-                button3.setBackgroundColor(Color.BLUE);
-        }
-
-        }
-
-
-    }
-
-    public void button1_Clicked(View v)
-    {
-        mix+=1;
-        x1++;
-        button1.setText(""+x1);
-    }
-    public void button2_Clicked(View v)
-    {
-        x2++;
-        mix+=1;
-        button2.setText(""+x2);
-    }
-    public void button3_Clicked(View v)
-    {
-        x3++;
-        mix+=1;
-        button3.setText(""+x3);
     }
 }
