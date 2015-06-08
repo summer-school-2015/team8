f336dc2a455b6bff580e877dbbf49fca10097288
MrVatnik <zacvars@gmail.com>
2015-06-08 13:26:29 +0300

    back buttons

--- project/LPlayer/src/com/example/LPlayer/LPlayer.java
+++ project/LPlayer/src/com/example/LPlayer/LPlayer.java
@@ -8,14 +8,32 @@ import android.graphics.Color;
 
 public class LPlayer extends Activity {
     private Button settingsbut=null;
+    private  Button infobut=null;
+    private Button setbackbut=null;
+    private Button infobackbut=null;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         settingsbut = (Button)findViewById(R.id.settingsbutton);
+        infobut = (Button)findViewById(R.id.infobut);
+        setbackbut = (Button)findViewById(R.id.setbackbut);
+        infobackbut =(Button)findViewById(R.id.infobackbut);
 
     }
     public void settingsbutton_Clicked(View v)
+    {
+        setContentView(R.layout.settings);
+    }
+    public void infobut_Clicked(View v)
+    {
+        setContentView(R.layout.info);
+    }
+    public void setbackbut_Clicked(View v)
+    {
+        setContentView(R.layout.main);
+    }
+    public void infobackbut_Clicked(View v)
     {
         setContentView(R.layout.settings);
     }
